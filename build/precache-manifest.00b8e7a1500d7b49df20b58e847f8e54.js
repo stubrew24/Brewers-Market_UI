374f0a0adf667a5e9273431929357677625b52d7
stubrew24 <sewelly@gmail.com>
2019-05-01 15:38:46 +0100

    Deploy

---
+++ build/precache-manifest.00b8e7a1500d7b49df20b58e847f8e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e01c05445c74d2ed902df0b43ade53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab49d4d60f3f5514c4b2",
+    "url": "/static/css/2.c489bcc4.chunk.css"
+  },
+  {
+    "revision": "fff47d3ac4a6943e8cba",
+    "url": "/static/css/main.72854ab5.chunk.css"
+  },
+  {
+    "revision": "ab49d4d60f3f5514c4b2",
+    "url": "/static/js/2.0a042fde.chunk.js"
+  },
+  {
+    "revision": "fff47d3ac4a6943e8cba",
+    "url": "/static/js/main.69db9821.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
--- src/API.js
+++ src/API.js
@@ -1,3 +1,3 @@
-const API_BASE = 'http://localhost:3000/api/v1/'
+const API_BASE = 'http://lit-taiga-20073.herokuapp.com/api/v1/'
 
 export { API_BASE }
